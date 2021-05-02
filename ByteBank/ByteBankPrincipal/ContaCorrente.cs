f4ff085327a9794430d63eb912a02a1c010afbee
NckAlves <nicholasoxalves@gmail.com>
2021-05-02 02:40:21 -0300

    Start!

---
+++ ByteBank/ByteBankPrincipal/ContaCorrente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankPrincipal
+{
+    public class ContaCorrente {
+
+        public string titular;
+        public int numeroAgencia;
+        public int numeroConta;
+        public double saldo;
+/*
+        public ContaCorrente(string titular, int numeroAgencia, int numeroConta, double saldo)
+        {
+            this.titular = titular;
+            this.numeroAgencia = numeroAgencia;
+            this.numeroConta = numeroConta;
+            this.saldo = saldo;
+        }
+*/
+    }
+
+
+}
---
+++ ByteBank/ByteBankPrincipal/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ByteBankPrincipal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            ContaCorrente joao = new ContaCorrente();
+            joao.saldo = 10;
+
+            Console.WriteLine(joao.saldo);
+            Console.WriteLine(joao.numeroConta);
+        }
+    }
+}
